03ea8f6f08721f2c6125159bfd6c57926250909a
cmarro3 <christophermarroquin352@gmail.com>
2022-02-17 15:19:55 -0600

    Update until next time

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  React.createElement("ul", null, 
+  React.createElement("li", null, 'Monday'),
+  React.createElement("li", null, 'Tuesday'),
+  React.createElement("li", null, 'Wednesday'),
+  React.createElement("li", null, 'Thursday'),
+  React.createElement("li", null, 'Fday'),
+  ),
+  document.getElementById("root")
 );
